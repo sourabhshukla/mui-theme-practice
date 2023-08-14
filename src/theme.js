bf3921b4ea79478384bc3c0fadd3136e389384f2
unknown <sourabhshukla9893502147@gmail.com>
2023-08-14 23:34:19 +0530

    When we don't define primary explicitly it takes default values

--- src/theme.js
+++ src/theme.js
@@ -4,9 +4,9 @@ import { orange } from "@mui/material/colors";
 const theme=createTheme({
     palette:{
         mode:'dark',
-        primary:{
-            main: orange[500]
-        }
+        // primary:{
+        //     main: orange[500]
+        // }
     },
     typography:{
         fontSize: 20
