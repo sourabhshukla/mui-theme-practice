8bba8701a424570a69841ff5ed80fc15192c96df
unknown <sourabhshukla9893502147@gmail.com>
2023-08-14 16:25:44 +0530

    Created some MUI components and implemented ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import { Button, Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
+      <Typography variant='h3' >Hello World!</Typography>
+      <Typography color='primary'>Hello MUI!</Typography>
+      <h3>Start Using MUI</h3>
+      <Button variant='contained' color='secondary'>
+        hi
+      </Button>
       
     </div>
   );
